cb6f49119ede1257abc29ad04f7c5318f55120c3
MasterMedo <mislav.vuletic@gmail.com>
2021-02-05 17:16:27 +0100

    107 - t: o(n), m: o(n)

---
+++ 107-binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+
+
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> list[list[int]]:
+        def traverse_and_add(root, depth=0):
+            nonlocal levels
+
+            if root is None:
+                return
+
+            levels[depth].append(root.val)
+            traverse_and_add(root.left, depth + 1)
+            traverse_and_add(root.right, depth + 1)
+
+        levels = defaultdict(list)
+        traverse_and_add(root)
+        return [levels[i] for i in range(len(levels) - 1, -1, -1)]
