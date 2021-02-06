52c680434974b8dd336e48029bc5b42cd63af4ae
MasterMedo <mislav.vuletic@gmail.com>
2021-02-06 18:37:25 +0100

    73 - t: o(nm), m: o(1)

---
+++ 73-set-matrix-zeroes.py
@@ -0,0 +1,25 @@
+class Solution:
+    def setZeroes(self, matrix: list[list[int]]) -> None:
+        is_col = False
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for r in range(rows):
+            if matrix[r][0] == 0:
+                is_col = True
+            for c in range(1, cols):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    matrix[r][0] = 0
+
+        for r in range(1, rows):
+            for c in range(1, cols):
+                if not matrix[r][0] or not matrix[0][c]:
+                    matrix[r][c] = 0
+
+        if matrix[0][0] == 0:
+            for c in range(cols):
+                matrix[0][c] = 0
+
+        if is_col:
+            for r in range(rows):
+                matrix[r][0] = 0
