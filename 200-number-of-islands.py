c63bf7c2d567fcc72740129a3b3e5f97686b450a
MasterMedo <mislav.vuletic@gmail.com>
2021-02-16 10:50:55 +0100

    200 - t: o(nm), m: o(nm)

--- 200-number-of-islands.py
+++ 200-number-of-islands.py
@@ -3,22 +3,18 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: list[list[str]]) -> int:
-        def sink(x, y):
-            grid[x][y] = 0
-            visit = deque([(x, y)])
-            while visit:
-                x, y = visit.popleft()
-                for dx, dy in ((0, -1), (0, 1), (1, 0), (-1, 0)):
-                    z, w = x + dx, y + dy
-                    if 0 <= z < len(grid) and 0 <= w < len(grid[0]) and grid[z][w] == '1':
-                        grid[z][w] = '0'
-                        visit.append((z, w))
-
         islands = 0
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if grid[x][y] == '1':
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == '1':
+                    grid[r][c] = '0'
                     islands += 1
-                    sink(x, y)
-
+                    visit = deque([(r, c)])
+                    while visit:
+                        x, y = visit.pop()
+                        for dx, dy in ((0, 1), (0, -1), (1, 0), (-1, 0)):
+                            nx, ny = x + dx, y + dy
+                            if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]) and grid[nx][ny] == '1':
+                                grid[nx][ny] = '0'
+                                visit.append((nx, ny))
         return islands
