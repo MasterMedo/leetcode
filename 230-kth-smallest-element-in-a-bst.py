83d9faa4aa4fb1a24995d4e729caf31c6c4fffbf
MasterMedo <mislav.vuletic@gmail.com>
2021-02-23 21:12:08 +0100

    230 - t: o(n), m: o(n)

--- 230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst.py
@@ -1,20 +1,9 @@
 class Solution:
     def kthSmallest(self, root: TreeNode, k: int) -> int:
-        def helper(node: TreeNode, smaller=0):
-            if node is None:
-                return False, 0, 0
+        def inorder(node: TreeNode) -> int:
+            if node is not None:
+                yield from inorder(node.left)
+                yield node.val
+                yield from inorder(node.right)
 
-            found, smaller_left, val = helper(node.left, smaller)
-            if found:
-                return True, 0, val
-
-            if smaller + smaller_left == k - 1:
-                return True, 0, node.val
-
-            found, smaller_right, val = helper(node.right, smaller + smaller_left + 1)
-            if found:
-                return True, 0, val
-
-            return False, smaller_left + smaller_right + 1, 0
-
-        return helper(root)[2]
+        return next(e for i, e in enumerate(inorder(root), 1) if i == k)
