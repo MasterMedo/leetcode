c3c51a9cf8e03242f3c95badfcb4c0c8fa9bdcfd
MasterMedo <mislav.vuletic@gmail.com>
2021-03-02 14:10:29 +0100

    1375 - t: o(n), m: o(1)

--- 1375-bulb-switcher-iii.py
+++ 1375-bulb-switcher-iii.py
@@ -1,20 +1,14 @@
-from heapq import heappush, heappop
-
-
 class Solution:
     def numTimesAllBlue(self, light: list[int]) -> int:
-        on = []
+        on = 0
+        max_ = 0
         blue = 0
-        moments = 0
         for switch in light:
-            if switch - 1 == blue:
+            on += 1
+            if switch > max_:
+                max_ = switch
+
+            if max_ == on:
                 blue += 1
-                while on and on[0] == blue + 1:
-                    blue = heappop(on)
-            else:
-                heappush(on, switch)
 
-            if not on:
-                moments += 1
-
-        return moments
+        return blue
