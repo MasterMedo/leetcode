21c58ce304312a1f8d743851a693043c957265b5
MasterMedo <mislav.vuletic@gmail.com>
2021-03-04 20:03:45 +0100

    986 - t: o(n + m), m: o(1)

--- 986-interval-list-intersections.py
+++ 986-interval-list-intersections.py
@@ -1,19 +1,15 @@
 class Solution:
     def intervalIntersection(self, a: list[list[int]], b: list[list[int]]) -> list[list[int]]:
-        result = []
+        intersections = []
         i = j = 0
         while i < len(a) and j < len(b):
-            loa, hia = a[i]
-            lob, hib = b[j]
-            if lob <= hia <= hib:
-                result.append([max(loa, lob), min(hia, hib)])
+            start = max(a[i][0], b[j][0])
+            end = min(a[i][1], b[j][1])
+            if start <= end:
+                intersections.append([start, end])
+            if a[i][1] < b[j][1]:
                 i += 1
-            elif loa <= hib <= hia:
-                result.append([max(loa, lob), min(hia, hib)])
-                j += 1
             else:
-                if hia < hib:
-                    i += 1
-                else:
-                    j += 1
-        return result
+                j += 1
+
+        return intersections
