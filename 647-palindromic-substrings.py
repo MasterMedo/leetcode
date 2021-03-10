744c2e709d363ae3d0531b0c06dc8684d6230faa
MasterMedo <mislav.vuletic@gmail.com>
2021-03-10 22:48:15 +0100

    647 - t: o(n^2), m: o(1)

--- 647-palindromic-substrings.py
+++ 647-palindromic-substrings.py
@@ -1,11 +1,16 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        seen = set()
         count = 0
-        for window in range(len(s)):
-            for lo in range(len(s) - window):
-                hi = lo + window
-                if s[lo] == s[hi] and (lo + 1 > hi - 1 or (lo + 1, hi - 1) in seen):
-                    count += 1
-                    seen.add((lo, hi))
+        for i in range(len(s)):
+            d = 0
+            while i - d >= 0 and i + d < len(s) and s[i - d] == s[i + d]:
+                count += 1
+                d += 1
+
+            lo, hi = i, i + 1
+            while lo >= 0 and hi < len(s) and s[lo] == s[hi]:
+                count += 1
+                lo -= 1
+                hi += 1
+
         return count
