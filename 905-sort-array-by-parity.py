ba3151e7b884b59c3fc217651d32bd01cbe392cd
MasterMedo <mislav.vuletic@gmail.com>
2021-04-09 14:57:57 +0200

    905 - t: o(n) m: o(1)

--- 905-sort-array-by-parity.py
+++ 905-sort-array-by-parity.py
@@ -1,3 +1,14 @@
 class Solution:
     def sortArrayByParity(self, arr: list[int]) -> list[int]:
-        return sorted(arr, key=lambda n: n & 1)
+        lo, hi = 0, len(arr) - 1
+        while lo < hi:
+            if not arr[lo] & 1:
+                lo += 1
+            elif arr[hi] & 1:
+                hi -= 1
+            else:
+                arr[lo], arr[hi] = arr[hi], arr[lo]
+                lo += 1
+                hi -= 1
+
+        return arr
