4037ad53c37c84d2a0c6e2a01db7ba3cb08e1b85
MasterMedo <mislav.vuletic@gmail.com>
2021-06-13 08:10:37 +0200

    1094 - t: o(nlogn), m: o(n)

--- 1094-car-pooling.py
+++ 1094-car-pooling.py
@@ -1,26 +1,14 @@
-from heapq import heappush, heappop
-
-
 class Solution:
     def carPooling(self, trips: list[list[int]], capacity: int) -> bool:
-        pick_up = []
-        drop_off = []
+        times = []
         for n, start, end in trips:
-            heappush(pick_up, (start, n))
-            heappush(drop_off, (end, n))
+            times.append((start, n))
+            times.append((end, -n))
 
+        times.sort()
         cap = 0
-        while drop_off or pick_up:
-            t = min(
-                pick_up and pick_up[0][0] or float('inf'),
-                drop_off and drop_off[0][0] or float('inf')
-            )
-            while pick_up and pick_up[0][0] <= t:
-                cap += heappop(pick_up)[1]
-
-            while drop_off and drop_off[0][0] <= t:
-                cap -= heappop(drop_off)[1]
-
+        for _, n in times:
+            cap += n
             if cap > capacity:
                 return False
 
