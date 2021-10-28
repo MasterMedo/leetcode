b64aade43764c31e39e04911d8ad14214bc5b6de
MasterMedo <mislav.vuletic@gmail.com>
2021-10-28 20:12:46 +0200

    94 - t: o(n), m: o(n)

---
+++ 94-binary-tree-inorder-traversal.py
@@ -0,0 +1,8 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        left = self.inorderTraversal(root.left)
+        right = self.inorderTraversal(root.right)
+        return left + [root.val] + right
