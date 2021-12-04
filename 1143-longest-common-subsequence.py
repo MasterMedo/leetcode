901a5c44e31453420ad7648f074cca762dfdca68
MasterMedo <mislav.vuletic@gmail.com>
2021-12-04 15:57:00 +0100

    1143 - t: o(nm), m: o(nm)

--- 1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence.py
@@ -1,11 +1,12 @@
 class Solution:
     def longestCommonSubsequence(self, a: str, b: str) -> int:
-        grid = [[0]*len(b) for _ in range(len(a))]
-        for i, c1 in enumerate(a):
-            for j, c2 in enumerate(b):
-                left = grid[i-1][j] if i != 0 else 0
-                up = grid[i][j-1] if j != 0 else 0
-                diag = grid[i-1][j-1] if i != 0 and j != 0 else 0
-                grid[i][j] = max(left, up, diag + (c1 == c2))
+        grid = [[0]*(len(b) + 1) for _ in range(len(a) + 1)]
+        for i in range(1, len(a) + 1):
+            for j in range(1, len(b) + 1):
+                grid[i][j] = max(
+                    grid[i-1][j],
+                    grid[i][j-1],
+                    grid[i-1][j-1] + (a[i - 1] == b[j - 1])
+                )
 
         return grid[-1][-1]
