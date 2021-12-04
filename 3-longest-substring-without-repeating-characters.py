e039da8d0cd112c41cf9548e5007425ea959b3d3
MasterMedo <mislav.vuletic@gmail.com>
2021-12-04 12:10:36 +0100

    3 - t: o(n), m: o(n)

--- 3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters.py
@@ -1,20 +1,12 @@
-from collections import deque
-
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = set()
-        length = 0
-        chars = deque()
-        for c in s:
+        seen = {}
+        max_ = 0
+        last = -1
+        for i, c in enumerate(s):
             if c in seen:
-                while True:
-                    last = chars.popleft()
-                    seen.remove(last)
-                    if last == c:
-                        break
-            chars.append(c)
-            seen.add(c)
-            length = max(length, len(chars))
+                last = max(last, seen[c])
 
-        return length
+            max_ = max(max_, i - last)
+            seen[c] = i
+        return max_
