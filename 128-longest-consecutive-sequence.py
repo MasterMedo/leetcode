1dafa506e2c3bc7a12919d52d96941adaca3135f
MasterMedo <mislav.vuletic@gmail.com>
2022-01-02 11:09:14 +0100

    128 - t: o(n), m: o(n)

--- 128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence.py
@@ -1,25 +1,13 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
+        nums = set(nums)
+        best = 0
+        for lowest in nums:
+            if lowest - 1 not in nums:
+                highest = lowest + 1
 
-        d = dict()
-        seen = set()
-        for n in nums:
-            if n in seen:
-                continue
-            seen.add(n)
+                while highest in nums:
+                    highest += 1
 
-            if n - 1 in d and n + 1 in d:
-                d[d[n - 1][0]][1] = d[n + 1][1]
-                d[d[n + 1][1]][0] = d[n - 1][0]
-            elif n - 1 in d:
-                d[n] = [d[n - 1][0], n]
-                d[d[n - 1][0]][1] = n
-            elif n + 1 in d:
-                d[n] = [n, d[n + 1][1]]
-                d[d[n + 1][1]][0] = n
-            else:
-                d[n] = [n, n]
-
-        return max(b - a + 1 for a, b in d.values())
+                best = max(best, highest - lowest)
+        return best
