605cf38a0e46562a55a9c5711bdca8f5876bbc1e
MasterMedo <mislav.vuletic@gmail.com>
2022-01-02 22:30:31 +0100

    delete 573 - wrong task submission

--- 573-complex-number-multiplication.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def complexNumberMultiply(self, a: str, b: str) -> str:
-        a = eval(a.replace('i', 'j'))
-        b = eval(b.replace('i', 'j'))
-        ab = a * b
-        return f'{ab.real:.0f}+{ab.imag:.0f}i'
