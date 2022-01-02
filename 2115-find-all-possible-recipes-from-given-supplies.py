300deb748901ba0742112ce34f5a3213cac5d00e
MasterMedo <mislav.vuletic@gmail.com>
2022-01-02 21:43:25 +0100

    2115 - t: o(nm), m: o(n)

--- 2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,41 +1,20 @@
 class Solution:
-    def findAllRecipes(
-        self,
-        recipes: List[str],
-        ingredients: List[List[str]],
-        supplies: List[str],
-    ) -> List[str]:
-        def is_recipe_reachable(i, seen):
-            nonlocal reachable_recipes, unreachable_recipes, supplies, indices
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        have = set(supplies)
+        want = set(range(len(recipes)))
+        need = set()
+        made = []
+        while len(need) != len(want):
+            need = want.copy()
+            for i in need:
+                can_make = True
+                for ingredient in ingredients[i]:
+                    if ingredient not in have:
+                        can_make = False
+                        break
 
-            if i in seen:
-                unreachable_recipes.add(i)
-                return False
-
-            for ingredient in ingredients[i]:
-                if ingredient not in supplies:
-                    if ingredient not in indices:
-                        unreachable_recipes.add(i)
-                        return False
-
-                    j = indices[ingredient]
-                    if j in unreachable_recipes:
-                        unreachable_recipes.add(i)
-                        return False
-
-                    if not is_recipe_reachable(j, seen | {i}):
-                        unreachable_recipes.add(i)
-                        unreachable_recipes.add(j)
-                        return False
-
-            return True
-
-        supplies = set(supplies)
-        indices = {recipes[i]: i for i in range(len(recipes))}
-        reachable_recipes = set()
-        unreachable_recipes = set()
-        for i in range(len(recipes)):
-            if is_recipe_reachable(i, set()):
-                reachable_recipes.add(i)
-
-        return [recipes[i] for i in reachable_recipes]
+                if can_make:
+                    made.append(recipes[i])
+                    have.add(recipes[i])
+                    want.remove(i)
+        return made
