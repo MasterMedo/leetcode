0318a917f51e803a3119cf90a545cefdf4e56333
MasterMedo <mislav.vuletic@gmail.com>
2022-01-02 22:23:34 +0100

    301 - t: o(n^2), m: o(n^2)

---
+++ 301-remove-invalid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def removeInvalidParentheses(self, s):
+        def isvalid(s):
+            cnt = 0
+            for c in s:
+                if c == '(':
+                    cnt += 1
+                elif c == ')':
+                    cnt -= 1
+                    if cnt < 0:
+                        return False
+            return cnt == 0
+
+        level = {s}
+        while True:
+            valid = list(filter(isvalid, level))
+            if valid:
+                return valid
+            level = {s[:i] + s[i+1:] for s in level for i in range(len(s))}
