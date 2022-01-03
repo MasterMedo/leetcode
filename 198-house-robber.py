b664c903d5a5cda37c94a773dc1575b8d3fd2ea2
MasterMedo <mislav.vuletic@gmail.com>
2022-01-03 20:07:08 +0100

    198 - t: o(n), m: o(1)

--- 198-house-robber.py
+++ 198-house-robber.py
@@ -1,7 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob_ = not_rob = 0
+        rob = not_rob = 0
         for n in nums:
-            rob_, not_rob = not_rob + n, max(rob_, not_rob)
+            rob, not_rob = not_rob + n, max(rob, not_rob)
 
-        return max(rob_, not_rob)
+        return max(rob, not_rob)
