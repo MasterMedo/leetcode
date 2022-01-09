f8229e57be298f4350b0f6c2ff179595e5d5ea68
MasterMedo <mislav.vuletic@gmail.com>
2022-01-09 09:58:58 +0100

    406 - t: o(nlogn), m: o(n)

--- 406-queue-reconstruction-by-height.py
+++ 406-queue-reconstruction-by-height.py
@@ -1,43 +1,41 @@
-def update(fenwick_tree, i, delta):
-    while i < len(fenwick_tree):
-        fenwick_tree[i] += delta
-        i += i & -i
+class FenwickTree:
+    def __init__(self, size):
+        self.size = size
+        self.tree = [0] * size
+        self.bit_mask = 1  # largest power of 2 smaller than (size - 1)
+        while self.bit_mask * 2 < size - 1:
+            self.bit_mask *= 2
 
+    def add(self, index, amount):
+        while index < self.size:
+            self.tree[index] += amount
+            index += index & -index
 
-def lower_bound(fenwick_tree, taller_in_front, bitMask):
-    ans_i = 0
-    while bitMask != 0:
-        fenwick_i = ans_i + bitMask
-        bitMask >>= 1
-        if fenwick_i < len(fenwick_tree):
-            if taller_in_front > fenwick_tree[fenwick_i]:
+    def get_index_of_sum(self, sum_) -> int:
+        ans_i = 0
+        bit_mask = self.bit_mask
+        while bit_mask != 0:
+            fenwick_i = ans_i + bit_mask
+            bit_mask //= 2
+            if fenwick_i < self.size and sum_ > self.tree[fenwick_i]:
                 ans_i = fenwick_i
-                taller_in_front -= fenwick_tree[fenwick_i]
+                sum_ -= self.tree[fenwick_i]
 
-    return ans_i
+        return ans_i if ans_i != self.size - 1 else ans_i - 1
 
 
 class Solution:
     def reconstructQueue(self, people: list[list[int]]) -> list[list[int]]:
-        if not people:
-            return []
-
         people.sort(key=lambda p: (p[0], -p[1]))
-        ans = [0] * len(people)
-        fenwick_tree = [0] * (len(people) + 1)
-        bitMask = 1
-        n = len(people)
-        while bitMask * 2 < n:
-            bitMask *= 2
+        ans = [None] * len(people)
+        fenwick_tree = FenwickTree(len(people) + 1)
 
-        for i in range(2, n + 1):
-            update(fenwick_tree, i, 1)
+        for i in range(2, len(people) + 1):
+            fenwick_tree.add(i, 1)
 
-        for people_i in range(n):
-            ans_i = lower_bound(fenwick_tree, people[people_i][1], bitMask)
-            if ans_i == len(people):
-                ans_i -= 1
-            ans[ans_i] = people[people_i]
-            update(fenwick_tree, ans_i + 1, -1)
+        for height, taller_in_front in people:
+            i = fenwick_tree.get_index_of_sum(taller_in_front)
+            ans[i] = [height, taller_in_front]
+            fenwick_tree.add(i + 1, -1)
 
         return ans
