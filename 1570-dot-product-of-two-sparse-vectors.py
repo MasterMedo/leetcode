0e4048551f0e0554076c25f62a25ae27315edcb8
MasterMedo <mislav.vuletic@gmail.com>
2022-01-24 20:33:32 +0100

    1570 - t: o(logn), m: o(logn)

--- 1570-dot-product-of-two-sparse-vectors.py
+++ 1570-dot-product-of-two-sparse-vectors.py
@@ -1,34 +1,6 @@
 class SparseVector:
     def __init__(self, nums: list[int]):
-        self.vals = []
-        self.length = len(nums)
-        cnt = 0
-        val = nums[0]
-        for n in nums:
-            if n == val:
-                cnt += 1
-            else:
-                self.vals.append([val, cnt])
-                val = n
-                cnt = 1
+        self.vals = {i: n for i, n in enumerate(nums) if n}
 
-        self.vals.append([val, cnt])
-
-    def dotProduct(self, vec: 'SparseVector') -> int:
-        arr1 = [x.copy() for x in self.vals]
-        arr2 = [x.copy() for x in vec.vals]
-        i1 = i2 = 0
-        product = 0
-        while i1 < len(arr1) and i2 < len(arr2):
-            n1, cnt1 = arr1[i1]
-            n2, cnt2 = arr2[i2]
-            cnt = min(cnt1, cnt2)
-            product += cnt * n1 * n2
-            arr1[i1][1] -= cnt
-            arr2[i2][1] -= cnt
-            if not arr1[i1][1]:
-                i1 += 1
-            if not arr2[i2][1]:
-                i2 += 1
-
-        return product
+    def dotProduct(self, vec: "SparseVector") -> int:
+        return sum(vec.vals[i] * self.vals[i] for i in vec.vals if i in self.vals)
