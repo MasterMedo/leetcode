b9d50e4921ec68d85e13bcd1c3c5c22237fcf0c5
MasterMedo <mislav.vuletic@gmail.com>
2022-01-29 15:06:23 +0100

    366 - t: o(n), m: o(logn)

--- 366-find-leaves-of-binary-tree.py
+++ 366-find-leaves-of-binary-tree.py
@@ -1,37 +1,17 @@
 class Solution:
     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
-        def traverse(node: TreeNode, parent: Optional[TreeNode] = None, left: bool = False) -> None:
-            nonlocal leaves
-            if node.left is None and node.right is None:
-                leaves.append(node)
-            else:
-                if node.left is not None:
-                    traverse(node.left, node, left=True)
-                if node.right is not None:
-                    traverse(node.right, node, left=False)
+        def traverse(node: Optional[TreeNode]) -> None:
+            nonlocal ans
+            if node is None:
+                return 0
 
-            node.parent = (parent, left)
+            i = max(traverse(node.left), traverse(node.right))
+            if i >= len(ans):
+                ans.append([])
 
-        leaves = []
-        traverse(root)
+            ans[i].append(node.val)
+            return i + 1
+
         ans = []
-        while leaves:
-            new_leaves = []
-            ans.append([])
-            for leaf in leaves:
-                ans[-1].append(leaf.val)
-                parent, left = leaf.parent
-                if parent is None:
-                    continue
-
-                if left:
-                    parent.left = None
-                else:
-                    parent.right = None
-
-                if parent.left is None and parent.right is None:
-                    new_leaves.append(parent)
-
-            leaves = new_leaves
-
+        traverse(root)
         return ans
