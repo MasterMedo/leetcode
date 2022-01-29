ca9bfa3fab75b0c7c91a027bfbe1fdd922e6a4d0
MasterMedo <mislav.vuletic@gmail.com>
2022-01-29 08:41:21 +0100

    35 - t: o(logn), m: o(1)

--- 35-search-insert-position.py
+++ 35-search-insert-position.py
@@ -1,6 +1,17 @@
-from bisect import bisect_left
-
-
 class Solution:
-    def searchInsert(self, nums: list[int], target: int) -> int:
-        return bisect_left(nums, target)
+    def searchInsert(self, arr: list[int], target: int) -> int:
+        if target > arr[-1]:
+            return len(arr)
+        lo = 0
+        hi = len(arr) - 1
+        while lo < hi:
+            mid = (lo + hi) // 2
+            n = arr[mid]
+            if n > target:
+                hi = mid
+            elif n < target:
+                lo = mid + 1
+            else:
+                return mid
+
+        return lo if arr[lo] >= target else lo - 1
