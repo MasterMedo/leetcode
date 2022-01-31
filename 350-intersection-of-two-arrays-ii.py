681ecf0c3de9a2ac01d53022ca8cb634552cbbbc
MasterMedo <mislav.vuletic@gmail.com>
2022-01-31 06:56:42 +0100

    350 - t: o(n), m: o(n)

--- 350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii.py
@@ -1,18 +1,14 @@
+from collections import defaultdict
+
+
 class Solution:
-    def intersect(self, a: list[int], b: list[int]) -> list[int]:
-        a.sort()
-        b.sort()
-        result = []
-        i = j = 0
-        while i < len(a) and j < len(b):
-            left = a[i]
-            right = b[j]
-            if left == right:
-                result.append(left)
-                i += 1
-                j += 1
-            elif left > right:
-                j += 1
-            else:
-                i += 1
-        return result
+    def intersect(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        d1 = defaultdict(int)
+        for n in nums1:
+            d1[n] += 1
+
+        d2 = defaultdict(int)
+        for n in nums2:
+            d2[n] += 1
+
+        return [n for n in d1 for _ in range(min(d1[n], d2[n]))]
