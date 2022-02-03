d0c3b7d8373a8eb2b5049b446d3fa24a5c09c617
MasterMedo <mislav.vuletic@gmail.com>
2022-02-03 06:45:34 +0100

    45 - t: o(n), m: o(1)

--- 45-jump-game-ii.py
+++ 45-jump-game-ii.py
@@ -1,13 +1,14 @@
-from math import inf
-
-
 class Solution:
-    def jump(self, arr: list[int]) -> int:
-        reachable = [inf] * len(arr)
-        reachable[0] = 0
-        for i in range(len(reachable)):
-            for j in range(i + 1, min(i + arr[i] + 1, len(arr))):
-                if reachable[j] > reachable[i]:
-                    reachable[j] = reachable[i] + 1
+    def jump(self, nums: List[int]) -> int:
+        prev_max_jump = 0
+        max_jump = nums[0]
+        jumps = 0
+        for i in range(1, len(nums)):
+            if i > prev_max_jump:
+                jumps += 1
+                prev_max_jump = max_jump
 
-        return reachable[-1]
+            if i + nums[i] > max_jump:
+                max_jump = i + nums[i]
+
+        return jumps
